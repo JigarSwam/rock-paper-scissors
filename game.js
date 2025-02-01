50db64793484b7a9db4ef03b18dda296378f6a88
JigarSwam <jswaminarayan@gmail.com>
2025-02-01 12:00:01 -0800

    implement 5 round game logic

--- game.js
+++ game.js
@@ -1,53 +1,49 @@
-// global score variables
-let humanScore = 0;
-let computerScore = 0;
-
-// get computer's choice and randomly return either "rock" "paper" or "scissors"
 
 function getComputerChoice() {
     let computerChoice;
     let result = Math.floor((Math.random() * 3));
     result === 0 ? computerChoice = "rock" : result === 1 ? computerChoice = "paper" : result === 2 ? computerChoice = "scissors" : ""
-    console.log("COMPUTER CHOICE: ", computerChoice);
     return computerChoice;
 }
 
-// get human player's choice
-// return one of the valid choices depending on what the user inputs
-
 function getHumanChoice() {
     let humanChoice = prompt("What is your choice?").toLowerCase();
-    console.log("HUMAN CHOICE: ", humanChoice);
     return humanChoice;
 }
 
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
 
-// play a single round with two parameters
-// humanChoice and computerChoice, use these to take the human and computer choices as args
-// write playRound function to console.log as a string value i.e.: "You lose! Paper beats Rock".
-// increment humanScore or computerScore variable based on the round winner
-
-function playRound(humanChoice, computerChoice) {
-    let result;
-    if (humanChoice === computerChoice) {
-        result = `Tie Game! You both picked ${humanChoice}`
-    } else if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
-        humanScore++;
-        result = `You win! ${humanChoice} beats ${computerChoice}`;
-    } else {
-        computerScore++;
-        result = `You lose! ${computerChoice} beats ${humanChoice}`;
+    function playRound(humanChoice, computerChoice) {
+        let result;
+        if (humanChoice === computerChoice) {
+            console.log(`Tie! You both picked ${humanChoice}`);
+        } else if (humanChoice === "rock" && computerChoice === "scissors" || 
+                    humanChoice === "paper" && computerChoice === "rock" || 
+                    humanChoice === "scissors" && computerChoice === "paper") {
+            humanScore++;
+            console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
+        } else {
+            computerScore++;
+            console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`);
+        }
+        return result;
     }
-    console.log("HUMAN SCORE:", humanScore);
-    console.log("COMPUTER SCORE:", computerScore);
-    return result;
+
+    for(let i = 1; i < 6; i++) {
+        console.log("ROUND", i);
+        playRound(getHumanChoice(), getComputerChoice());
+    }
+
+    if(humanScore > computerScore) {
+        console.log(`You've won!\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`);
+    } else if (humanScore == computerScore) {
+        console.log(`Tie Game!\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`)
+    } else {
+        console.log(`You lost :(\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`)
+    }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-console.log(playRound(humanSelection, computerSelection));
-
-// play the entire game
-// play 5 rounds in a function called playGame which calls playRound 5 times
+playGame();
