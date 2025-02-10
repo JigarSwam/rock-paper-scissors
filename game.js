9128bf12fa5feae71daeadc6dbd935035a881818
JigarSwam <jswaminarayan@gmail.com>
2025-02-10 11:45:10 -0800

    created gui and refactored game logic

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector("#btn");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+const scoreboard = document.querySelector(".scoreboard");
+scoreboard.textContent = `Score:\nHuman: ${humanScore}\nComputer: ${computerScore}`
+
 
 function getComputerChoice() {
     let computerChoice;
@@ -6,44 +15,55 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("What is your choice?").toLowerCase();
-    return humanChoice;
-}
+rockBtn.addEventListener("click", () => {
+    playRound("rock", getComputerChoice());
+});
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
+paperBtn.addEventListener("click", () => {
+    playRound("paper", getComputerChoice());
+})
 
-    function playRound(humanChoice, computerChoice) {
-        let result;
-        if (humanChoice === computerChoice) {
-            console.log(`Tie! You both picked ${humanChoice}`);
-        } else if (humanChoice === "rock" && computerChoice === "scissors" || 
-                    humanChoice === "paper" && computerChoice === "rock" || 
-                    humanChoice === "scissors" && computerChoice === "paper") {
-            humanScore++;
-            console.log(`You win this round! ${humanChoice} beats ${computerChoice}`);
-        } else {
-            computerScore++;
-            console.log(`You lose this round! ${computerChoice} beats ${humanChoice}`);
-        }
-        return result;
-    }
+scissorsBtn.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice());
+})
 
-    for(let i = 1; i < 6; i++) {
-        console.log("ROUND", i);
-        playRound(getHumanChoice(), getComputerChoice());
-    }
+function playRound(humanChoice, computerChoice) {
+    let roundResult;
+    let resultDiv = document.createElement("div");
 
-    if(humanScore > computerScore) {
-        console.log(`You've won!\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`);
-    } else if (humanScore == computerScore) {
-        console.log(`Tie Game!\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`)
+    if (humanChoice === computerChoice) {
+        roundResult = `Tie! You both picked ${humanChoice}`;
+    } else if (humanChoice === "rock" && computerChoice === "scissors" || 
+                humanChoice === "paper" && computerChoice === "rock" || 
+                humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore++;
+        roundResult = `You win this round! ${humanChoice} beats ${computerChoice}`;
     } else {
-        console.log(`You lost :(\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`)
+        computerScore++;
+        roundResult = `You lose this round! ${computerChoice} beats ${humanChoice}`;
     }
-
+    scoreboard.textContent = `Score: Human: ${humanScore} Computer: ${computerScore}`;
+    resultDiv.textContent = roundResult;
+    scoreboard.appendChild(resultDiv);
+    if (humanScore > 4 || computerScore > 4) {
+        rockBtn.removeEventListener("click", () => {
+            playRound();
+        });
+        paperBtn.removeEventListener("click", () => {
+            playRound();
+        });
+        scissorsBtn.removeEventListener("click", () => {
+            playRound();
+        });
+    
+        let gameResultDiv = document.createElement("div");
+        
+        if(humanScore > computerScore) {
+            gameResultDiv.textContent = `You've won!\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`;
+        } else {
+            gameResultDiv.textContent = `You lost :(\nFinal Score:\n You: ${humanScore}\n Computer: ${computerScore}`;
+        }
+        scoreboard.appendChild(gameResultDiv);
+    }
+    return roundResult;
 }
-
-playGame();
